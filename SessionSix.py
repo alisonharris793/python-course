964f98f84773a6645798ee58989a90f9114dc72f
alisonharris793 <63002302+alisonharris793@users.noreply.github.com>
2023-03-14 20:46:40 +0000

    Added a function to play the game three times. Added a function to keep hold of the overall score and return who the overall winner is of all three rounds.

--- SessionSix.py
+++ SessionSix.py
@@ -1,6 +1,7 @@
 import random
 import requests
 
+# this is a function that calls the pokemon API and returns four stats from a random pokemon
 def random_pokemon():
     pokemon_number = random.randint(1, 151)
     url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
@@ -13,19 +14,40 @@ def random_pokemon():
     'weight': pokemon['weight'],
     }
 
+# this is a function that runs the game three times and keeps a hold of the overall score
 def run():
-    my_pokemon = random_pokemon()
-    print('You were given {}'.format(my_pokemon['name']))
-    stat_choice = input('Which stat do you want to use? (id, height, weight) ')
-    opponent_pokemon = random_pokemon()
-    print('The opponent chose {}'.format(opponent_pokemon['name']))
-    my_stat = my_pokemon[stat_choice]
-    opponent_stat = opponent_pokemon[stat_choice]
-    if my_stat > opponent_stat:
-        print('You Win!')
-    elif my_stat < opponent_stat:
-        print('You Lose!')
+    score = 0
+    for x in range(3):
+        my_pokemon = random_pokemon()
+        print('You were given {}'.format(my_pokemon['name']))
+        stat_choice = input('Which stat do you want to use? (id, height, weight) ')
+        opponent_pokemon = random_pokemon()
+        print('The opponent chose {}'.format(opponent_pokemon['name']))
+        outcome = compare_stats(my_pokemon[stat_choice], opponent_pokemon[stat_choice])
+        if outcome == True:
+            score += 1
+    calculate_win(score)
+
+# this is a function to calculate whether my score is enough to win overall
+def calculate_win(score):
+    if score >= 2:
+        print('You are the overall winner!')
     else:
-        print('Draw!')
+        print('You have lost!')
+
+
+# this is a function to compare the stats of both pokemon
+# this will return true or false depending on the outcome of the compare_stats
+def compare_stats(my_stat, opponent_stat):
+    if my_stat > opponent_stat:
+        print('You Win This Round!')
+        return True
+    elif my_stat < opponent_stat:
+        print('You Lose This Round!')
+        return False
+    else:
+        print('Draw On This Round!')
+        return False
+
 
 run()
